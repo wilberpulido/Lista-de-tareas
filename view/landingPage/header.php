2b59c644a9287ec28462a877532ce7ae9a4335f0
unknown <pulidowilber@gmail.com>
2021-01-02 02:50:39 -0300

    avance en el diseño para celulares

--- view/landingPage/header.php
+++ view/landingPage/header.php
@@ -1,4 +1,4 @@
-<ul class="narbar navbar-dark bg-dark row">
+<ul class="navegbar navbar-dark bg-dark">
     <li>
         <a href="<?php echo $whyUseUrl ?>">
             Why this application?
@@ -12,16 +12,6 @@
     <li>
         <a href="<?php echo $contactUrl ?>">
             Contact
-        </a>
-    </li>
-    <li>
-        <a href="<?php echo $loginUrl ?>">
-            Login
-        </a>
-    </li>
-    <li>
-        <a href="<?php echo $registerUrl ?>">
-            Register
         </a>
     </li>
 </ul>
