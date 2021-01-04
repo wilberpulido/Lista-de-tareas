98d8e1a81c3f8dcca3072ab066c717e7ff71194a
unknown <pulidowilber@gmail.com>
2021-01-04 18:13:20 -0300

    Termine login, inicie formulario de registro

--- view/contact.php
+++ view/contact.php
@@ -31,37 +31,62 @@
         </div>
     </header>
     <main>
-        <section id="contactPage">
-            <div>
-                <h2 class="text-center mb-5">
-                    Contact form
+        <div id="contactPage">
+            <section>
+                <h2 class="text-center">
+                    Introduction
                 </h2>
                 <p class="text-justify">
                     This application has been developed by Wilber Pulido,
-                    a web developer with experience in different languages such as 
+                    web developer with experience in different languages such as 
                     html, css, php, java, javascript with react.js and express.js and handling mySQL and mongoDB
                     databases.
                 </p>
-                <form id="formContact" method="POST" action="../services/contactMail.php">
-                    <div class="container-label-input">
-                        <label for="nameContact">Name: </label>
-                        <input type="text" name="nameContact" id="nameContact" required>
-                        <label for="telephoneContact">Telephone: </label>
-                        <input type="tel" name="telephoneContact" id="telephoneContact" required>
+                <div id="contactInf">
+                    <h2 class="text-center">Contact information</h2>
+                    <div>
+                        <a href="https://www.linkedin.com/in/wilber-pulido-b523116b/" target="_blank">
+                            <i class="fab fa-linkedin fa-7x"></i>
+                         </a>
                     </div>
-                    <div class="container-label-input">
-                        <label for="emailContact">Email: </label>
-                        <input type="email" name="emailContact" id="emailContact" required>
+                    <div>
+                        <a href="https://wa.link/atmsgr" target="_blank">
+                            <i class="fab fa-whatsapp fa-7x"></i>
+                        </a>
                     </div>
+                    <h2 class="text-center">Other websites</h2>
+                    <div class="contactGit">
+                        <a href="https://github.com/wilberpulido" target="_blank">
+                            <i class="fab fa-github fa-7x"></i>
+                        </a>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <h2 class="text-center">
+                    Contact form
+                </h2>
+                <form method="POST" action="../services/contactMail.php">
+                <div  id="formContact">
+                    <label for="nameContact">Name: </label>
+                    <input type="text" name="nameContact" id="nameContact" required>
+
+                    <label for="telephoneContact">Telephone: </label>
+                    <input type="tel" name="telephoneContact" id="telephoneContact" required>
+
+                    <label for="emailContact">Email: </label>
+                    <input type="email" name="emailContact" id="emailContact" required>
+
                     <div>
                         <label for="messageContact">Message: </label>
                         <textarea name="messageContact" id="messageContact" required></textarea>
                     </div>
-                    <button class="btn btn-dark" type="submit">SEND MESSAGE</button>
-                </form>
+                        <button class="btn btn-dark" type="submit">SEND MESSAGE</button>
+                    </div>
+                    </form>
                 <?php 
                     if (isset($_SESSION["message"])) {
-                    
+                        
                         if($_SESSION["message"]){
                             echo "<div class = 'd-flex justify-content-center'>";
                             echo "<h3 class = 'mt-4 btn btn-success'> message sent successfully. </h3>";
@@ -79,30 +104,9 @@
                         }
                     }
                 ?>
-            </div>
-            <div id="personalDate">
-                <h3 class="text-center">Contact information</h3>
-                <div class="contactInf">
-                    <a class="icon" href="https://www.linkedin.com/in/wilber-pulido-b523116b/" target="_blank">
-                        <i class="fab fa-linkedin fa-5x"></i>
-                    </a>
-                    <a class="icon" href="https://wa.link/atmsgr" target="_blank">
-                        <i class="fab fa-whatsapp fa-5x"></i>
-                    </a>
-                </div>
-                <h3 class="text-center">Other websites</h3>
-                <div  class="contactInf">
-                    <a class="icon" href="https://github.com/wilberpulido" target="_blank">
-                        <i class="fab fa-github fa-5x"></i>
-                    </a>
-
-                </div>
-
-            </div>
-        </section>
+            </section>
+        </div>
     </main>
-
-    </div>
-    
+</div>
 </body>
 </html>
