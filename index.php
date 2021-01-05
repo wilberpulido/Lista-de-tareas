2f351f224e2955e960e0548c9d1e72ecb6d8e38a
unknown <pulidowilber@gmail.com>
2021-01-05 20:57:04 -0300

    primeros arregles del formulario de add tareas.

--- index.php
+++ index.php
@@ -24,7 +24,9 @@ if (isset($_SESSION["idUser"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale = 1, user-scalable=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/form.css">
     <link rel="stylesheet" href="style/style.css">
+    <script src="https://kit.fontawesome.com/ae8e09c608.js" crossorigin="anonymous"></script>
     <link rel="shortcut icon" href="images/iconTask.icon"/>
     <script defer type="text/javascript" src="./funciones.js"></script>
     <title>Home page</title>
@@ -66,80 +68,79 @@ if (empty($user)) {
 <div class= 'container-fluid'>
     <header>
         <div class="nav-profile bg-dark">
-            <div class='col-3'>
-                </div>
-                <div class='col-6 text-center'>
-                    <h2 class='text-white'> WELCOME <?php echo $user['firstName']." ".$user['lastName']."</h2>";?>
-                </div>
-                <div class= 'col-3 text-right'>
-                    <a class='text-white' href="controller/logout.php" >LOGOUT</a>
-                </div>
+            <div>
+                <h2> WELCOME <?php echo $user['firstName']." ".$user['lastName']."</h2>";?>
+            </div>
+            <div>
+                <a href="controller/logout.php" >LOGOUT</a>
+            </div>
         </div>
     </header>
-    <main class="mb-5">
-        <div class="row">
-            <div class="col-3 form">
-                <?php
-                    include_once("view/landingPage/formTask.html");
-                ?>
-            </div>
-            <div class="col-9 tasksBox">
-                <h3 class="text-center mt-4 mb-4"> THESE ARE THE TASKS </h3>
-                <div class="row taskRow">
-                    <div class="col-3 text-center">
-                        <h4>Task</h4>
-                    </div>
-                    <div class="col-2 text-center">
-                        <h4>State</h4>
-                    </div>
-                    <div class="col-2 text-center">
-                        <h4>Priority</h4>
-                    </div>
-                    <div class="col-2 text-center">
-                        <h4>Deadline</h4>
-                    </div>
+    <main>
+        <button class="btn btn-dark btn-add-task-form">
+            add task
+        </button>
+        <div class="formBoxTask">
+            <?php
+                include_once("view/landingPage/formTask.html");
+            ?>
+        </div>
+        <div class="tasksBox">
+            <h3 class="text-center mt-4 mb-4"> THESE ARE THE TASKS </h3>
+            <div class="row taskRow">
+                <div class="text-center">
+                    <h4>Task</h4>
                 </div>
-                <div class="listTask">
-                <?php
-                while ($row = mysqli_fetch_assoc($taskResult)) {
+                <div class="text-center">
+                    <h4>State</h4>
+                </div>
+                <div class="text-center">
+                    <h4>Priority</h4>
+                </div>
+                <div class="text-center">
+                    <h4>Deadline</h4>
+                </div>
+            </div>
+            <div class="listTask">
+            <?php
+            while ($row = mysqli_fetch_assoc($taskResult)) {
+            
+                $urlDelete = "services/deleteTask.php?idTask=".$row['idTask'];
+                $urlEdit = "view/editTaskForm.php?idTask=".$row['idTask'];
+            ?>
+            <br>
+            <div class="row taskRow">
+                <div class="col-3 taskIndividualBox">
+                    <p>
+                        <?php echo $row['task']?>
+                    </p>
+                </div>
+                <div class="col-2 text-center">
+                    <p>
+                        <?php echo $row['state']?>
+                    </p>
+                </div>
+                <div class="col-2 text-center">
+                    <p>
+                        <?php echo $row['priority']?>
+                    </p>
+                </div>
+                <div class="col-2 text-center">
+                    <p>
+                        <?php echo $row['deadline']?>
+                    </p>
+                </div>
+                <div class="col-3 text-center">
+                    <a href="<?php echo $urlEdit?>" class="btn btn-dark"> Edit </a>
+                    <a href="<?php echo $urlDelete?>" class="btn btn-dark">Delete</a><?php ?>
+                </div>
                 
-                    $urlDelete = "services/deleteTask.php?idTask=".$row['idTask'];
-                    $urlEdit = "view/editTaskForm.php?idTask=".$row['idTask'];
-                ?>
-                <br>
-                <div class="row taskRow">
-                    <div class="col-3 taskIndividualBox">
-                        <p>
-                            <?php echo $row['task']?>
-                        </p>
-                    </div>
-                    <div class="col-2 text-center">
-                        <p>
-                            <?php echo $row['state']?>
-                        </p>
-                    </div>
-                    <div class="col-2 text-center">
-                        <p>
-                            <?php echo $row['priority']?>
-                        </p>
-                    </div>
-                    <div class="col-2 text-center">
-                        <p>
-                            <?php echo $row['deadline']?>
-                        </p>
-                    </div>
-                    <div class="col-3 text-center">
-                        <a href="<?php echo $urlEdit?>" class="btn btn-dark"> Edit </a>
-                        <a href="<?php echo $urlDelete?>" class="btn btn-dark">Delete</a><?php ?>
-                    </div>
-                    
-                </div>
-                <br>
-                <?php
-                }
-                ?>
             </div>
-            </div>
+            <br>
+            <?php
+            }
+            ?>
+        </div>
         </div>
     </main>
 
