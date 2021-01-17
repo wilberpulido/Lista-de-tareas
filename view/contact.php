1e2d0b64fa398e65c49f9acb34207f1cb84de61a
unknown <pulidowilber@gmail.com>
2021-01-17 18:30:31 -0300

    adelante responsive tablet y laptop

--- view/contact.php
+++ view/contact.php
@@ -7,8 +7,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="../style/style.css">
     <link rel="stylesheet" href="../style/form.css">
+    <link rel="stylesheet" href="../style/style.css">
     <link rel="shortcut icon" href="../images/iconTask.icon"/>
     <script src="https://kit.fontawesome.com/ae8e09c608.js" crossorigin="anonymous"></script>
     <script defer type="text/javascript" src="../funciones.js"></script>
@@ -47,18 +47,18 @@
                     <h2 class="text-center">Contact information</h2>
                     <div>
                         <a href="https://www.linkedin.com/in/wilber-pulido-b523116b/" target="_blank">
-                            <i class="fab fa-linkedin fa-7x"></i>
+                            <i class="fab fa-linkedin fa-5x"></i>
                          </a>
                     </div>
                     <div>
                         <a href="https://wa.link/atmsgr" target="_blank">
-                            <i class="fab fa-whatsapp fa-7x"></i>
+                            <i class="fab fa-whatsapp fa-5x"></i>
                         </a>
                     </div>
                     <h2 class="text-center">Other websites</h2>
                     <div class="contactGit">
                         <a href="https://github.com/wilberpulido" target="_blank">
-                            <i class="fab fa-github fa-7x"></i>
+                            <i class="fab fa-github fa-5x"></i>
                         </a>
                     </div>
                 </div>
@@ -68,23 +68,25 @@
                     Contact form
                 </h2>
                 <form method="POST" action="../services/contactMail.php">
-                <div  id="formContact">
-                    <label for="nameContact">Name: </label>
-                    <input type="text" name="nameContact" id="nameContact" required>
+                    <div  id="formContact">
+                        <label for="nameContact">Name: </label>
+                        <input type="text" name="nameContact" id="nameContact" required>
 
-                    <label for="telephoneContact">Telephone: </label>
-                    <input type="tel" name="telephoneContact" id="telephoneContact" required>
+                        <label for="telephoneContact">Telephone: </label>
+                        <input type="tel" name="telephoneContact" id="telephoneContact" required>
 
-                    <label for="emailContact">Email: </label>
-                    <input type="email" name="emailContact" id="emailContact" required>
+                        <label for="emailContact">Email: </label>
+                        <input type="email" name="emailContact" id="emailContact" required>
 
-                    <div>
-                        <label for="messageContact">Message: </label>
-                        <textarea name="messageContact" id="messageContact" required></textarea>
+                        <div>
+                            <label for="messageContact">Message: </label>
+                            <textarea name="messageContact" id="messageContact" required></textarea>
+                        </div>
+                        <div class="btn-submit">
+                            <button class="btn" type="submit">SEND MESSAGE</button>
+                        </div>
                     </div>
-                        <button class="btn" type="submit">SEND MESSAGE</button>
-                    </div>
-                    </form>
+                </form>
                 <?php 
                     if (isset($_SESSION["message"])) {
                         
