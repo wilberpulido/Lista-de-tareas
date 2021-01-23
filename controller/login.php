310c0dcd107ca1f141175f6d085ebab6331088ff
unknown <pulidowilber@gmail.com>
2021-01-22 21:16:54 -0300

    termine login, posat

--- controller/login.php
+++ controller/login.php
@@ -9,17 +9,22 @@ $query= "SELECT * from users WHERE username = "."'".$_POST['username']."' OR mai
 $result = mysqli_query($connect->conn,$query);
 
 if ($result->num_rows > 0) {
-    
+
     while ($fila = mysqli_fetch_assoc($result)) {
 
         if (password_verify($_POST['password'],$fila["password"])) {
             $_SESSION["idUser"] = $fila['idUser'];
             return header("location: ../index.php");
-        }
+        }else {
+            echo '<script type="text/javascript"> alert("The user or password entered does not match") </script>';
+        
+            include_once("../view/login.html");
+        
+          }
     }
 
 } else {
-    echo '<script type="text/javascript"> alert("The user or password entered does not match") </script>';
+    echo '<script type="text/javascript"> alert("The user entered does not match") </script>';
 
     include_once("../view/login.html");
 
