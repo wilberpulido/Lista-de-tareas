7bebe9994ab89a69410c07db43bbf4dfae9f92eb
unknown <pulidowilber@gmail.com>
2021-02-11 23:53:30 -0300

    header and fooder finish

--- view/partials/header.php
+++ view/partials/header.php
@@ -1,23 +1,59 @@
-<ul class="navegbar">
-    <li >
-        <button>
+<div id="menu-short">
+    <button class="btn btn-menu"> Menu</button>
+    <div class="other-submenu">
+        <button class="btn"> <a href="view/login.html"> Login </a> </button> 
+        <button class="btn"> <a href="view/registration.html"> Register </a></button>
+    </div>
+    <ul class="navegbar">
+        <li >
+            <button>
+                <a href="<?php echo $whyUseUrl ?>">
+                    Why this application?
+                </a>
+            </button>
+        </li>
+        <li>
+            <button>
+                <a href="<?php echo $aboutMeUrl ?>">
+                    About me
+                </a>
+            </button>
+        </li>
+        <li>
+            <button>
+            <a href="<?php echo $contactUrl ?>">
+                Contact
+            </a>
+            </button>
+        </li>
+    </ul>
+</div>
+<div id="menu-large">
+    <ul class="navegbar-desktop">
+        <li >
             <a href="<?php echo $whyUseUrl ?>">
                 Why this application?
             </a>
-        </button>
-    </li>
-    <li>
-        <button>
+        </li>
+        <li>
             <a href="<?php echo $aboutMeUrl ?>">
                 About me
             </a>
-        </button>
-    </li>
-    <li>
-        <button>
-        <a href="<?php echo $contactUrl ?>">
-            Contact
-        </a>
-        </button>
-    </li>
-</ul>
+        </li>
+        <li>
+            <a href="<?php echo $contactUrl ?>">
+                Contact
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo $loginUrl ?>">
+                Login
+            </a>
+        </li>
+        <li>
+            <a href="<?php echo $registerUrl ?>">
+                Register
+            </a>
+        </li>
+    </ul>
+</div>
